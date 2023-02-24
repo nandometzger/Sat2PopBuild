fef9164453d0199c2aaae635c3f2ba85d3fdae4a
nandometzger <metzgern@ethz.ch>
2023-02-24 18:56:02 +0100

    push updates to script

--- regression/src/download_more_vars.py
+++ regression/src/download_more_vars.py
@@ -46,24 +46,21 @@ rename_countries = {"Czechia": "Czech_Republic"}
 
 # done_cities = ["zagreb", "budapest", "riga", "bari"]
 MSdone_cities = ["zagreb", "budapest", "riga", "bari"]
-S1done_cities = []
+S1done_cities = ["leipzig"]
 # gcloud auth login --remote-bootstrap="https://accounts.google.com/o/oauth2/auth?response_type=code&client_id=517222506229-vsmmajv00ul0bs7p89v5m89qs8eb9359.apps.googleusercontent.com&redirect_uri=https%3A%2F%2Fsdk.cloud.google.com%2Fapplicationdefaultauthcode.html&scope=https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fearthengine+https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fdevstorage.full_control+https%3A%2F%2Fwww.googleapis.com%2Fauth%2Faccounts.reauth&state=JxOD4NAMO70eVCK3e5VK5Ga53M2CGF&prompt=consent&access_type=offline&code_challenge=JqIR_YDSAVUNu5pkOruqBeB84xNrT0mXAbTGvWmdlxU&code_challenge_method=S256"
 
 Sentinel1_start_date = '2017-07-03'
-Sentinel1_finish_date = '2017-07-30'
-orbit = 'ASCENDING'
+Sentinel1_finish_date = '2017-08-30'
+orbit = 'DESCENDING'
 
 def get_country_name(file_name): 
 
-    with rasterio.open(file_name) as src:
-        # band1 = src.read(1)
+    with rasterio.open(file_name) as src: 
         height = src.height
         width = src.width
         this_crs = src.crs
 
-    # get the coordinates in their local coodinate system
-    # height = band1.shape[0]
-    # width = band1.shape[1]
+    # get the coordinates in their local coodinate system 
     cols, rows = np.meshgrid(np.arange(width), np.arange(height))
     xs, ys = rasterio.transform.xy(src.transform, rows, cols)
     lons = np.array(xs)
@@ -103,6 +100,7 @@ def extend_dataset(path, data_to_download=["MSBuildings"]):
             continue
         data_path = join(each_city, "sen2spring")
         data_pathMSB = join(each_city, "MSB")
+        data_pathS1 = join(each_city, "S1")
         csv_path = join(each_city, each_city.split(sep)[-1:][0] + '.csv')
         city_df = pd.read_csv(csv_path)
         ids = city_df['GRD_ID']
@@ -111,37 +109,30 @@ def extend_dataset(path, data_to_download=["MSBuildings"]):
         classes_str = [str(x) for x in classes]
         classes_paths = [data_path + '/Class_' + x + '/' for x in classes_str]
         classes_pathsMSB = [data_pathMSB + '/Class_' + x + '/' for x in classes_str]
+        classes_pathsS1 = [data_pathS1 + '/Class_' + x + '/' for x in classes_str]
         city = each_city.split("_")[-1] 
         
         country_name,_ = get_country_name(classes_paths[0] + str(ids[0]) + '_sen2spring.tif')
 
-        if country_name in medium:
-            print("please check if there is really data available")
-        # MSB Footprints
-        # if country_name in MSB_no_data_countries:
-        #     # print("There is no MSB data for:", country_name)
-        #     continue
-
-        # if country_name is None:
-        #     # there are some cities where countryname== is still oke, because they are by the sea
-        #     if city in manually_checked_cities.keys():
-        #         pass
-        #         country_name = manually_checked_cities[city]
-        #     else:
-        #         continue
         print(city, ",", country_name)
 
-        # if city in done_cities:
-        #     continue
+        if country_name in medium:
+            print("please check if there is really data available")  
+            # raise Exception("TODO: Still need to check the availablitiy of MSB footprints for cities here")
+        else:
+            continue
+
 
         for index in tqdm(range(0, len(classes_paths))):
             file_name = classes_paths[index] + str(ids[index]) + '_sen2spring.tif'
 
             name_MSB = str(ids[index]) + '_MSB'
+            name_S1 = str(ids[index]) + '_S1'
             file_name_MSB = classes_pathsMSB[index] + str(ids[index]) + '_MSB'
+            file_name_S1 = classes_pathsS1[index] + str(ids[index]) + '_S1'
 
 
-            #MSB Footprints
+            #check if the MSB Footprints
             download_MSB = True
             if country_name in MSB_no_data_countries:
                 download_MSB = False
@@ -161,20 +152,24 @@ def extend_dataset(path, data_to_download=["MSBuildings"]):
                 download_MSB = False
 
             
-            download_S1 = True # TODO: change later
+            # Check if downloading of S1 is needed
+            download_S1 = True 
+            if city in S1done_cities:
+                download_S1 = False
+
+
             if download_MSB or download_S1: 
                 this_country, exportarea = get_country_name(file_name)
                 this_country = country_name if this_country is None else this_country
-
 
             if download_MSB:
                 Bfeature = ee.FeatureCollection('projects/sat-io/open-datasets/MSBuildings/'+this_country)
                 B_filtered = Bfeature.filterBounds(exportarea)
                 task = ee.batch.Export.table.toDrive(
                     collection = B_filtered,
-                    description = name_MSB+"_"+city,
+                    description = name_MSB + "_" + city,
                     fileFormat = 'GeoJSON',
-                    folder = "So2Satdata/"+classes_pathsMSB[index], 
+                    folder = "So2Satdata/" + classes_pathsMSB[index], 
                 )
                 tasks.append(task)
                 task.start()
@@ -183,24 +178,37 @@ def extend_dataset(path, data_to_download=["MSBuildings"]):
                 task_id +=1
 
             if download_S1:
+
                 collectionS1 = ee.ImageCollection('COPERNICUS/S1_GRD')
-                collectionS1 = collectionS1.filter(ee.Filter.listContains('transmitterReceiverPolarisation', 'VV'))
                 collectionS1 = collectionS1.filter(ee.Filter.eq('instrumentMode', 'IW'))
-                collectionS1 = collectionS1.filterDate(Sentinel1_start_date, Sentinel1_finish_date)
+                collectionS1 = collectionS1.filter(ee.Filter.listContains('transmitterReceiverPolarisation', 'VV'))
+                collectionS1 = collectionS1.filter(ee.Filter.listContains('transmitterReceiverPolarisation', 'VH'))
+                collectionS1 = collectionS1.filter(ee.Filter.eq('orbitProperties_pass', orbit))
                 collectionS1 = collectionS1.filterBounds(exportarea)
-                collectionS1 = collectionS1.first()
-                # collectionS1 = collectionS1.clip(exportarea)
+                collectionS1 = collectionS1.filter(ee.Filter.contains('.geo', exportarea));
+                collectionS1 = collectionS1.filterDate(Sentinel1_start_date, Sentinel1_finish_date)
+                collectionS1 = collectionS1.select(['VV', 'VH'])
+                # collectionS1_first = ee.Image(collectionS1.sort("", False).first())
+                # collectionS1_first = ee.Image(collectionS1.sort("system:time_start", False).first())
+                # collectionS1_first = ee.Image(ee.List(collectionS1.toList(999)).get(0))
+                collectionS1_first = collectionS1.median()
+                # collectionS1_first = collectionS1.first()
+                # collectionS1_first = collectionS1.mosaic()
 
-                task_config = {
-                    'description': 'imageToDriveExample',
-                    'scale': 30,  
-                    'region': exportarea
-                }
+                task = ee.batch.Export.image.toDrive(
+                    image = collectionS1_first,
+                    scale = 10,  
+                    description = name_S1 + "_" + city,
+                    fileFormat="GEOTIFF", 
+                    folder = "So2Satdata/" + classes_pathsS1[index], 
+                    region = exportarea,
+                    crs='EPSG:4326'
+                )
+                tasks.append(task)
+                task.start()
+                task.status()
 
-                task = ee.batch.Export.image(collectionS1, 'exportExample', task_config)
-
-
-            # TODO: Download Sentinel 1 as well!! should be quick!
+                task_id +=1
 
             if (task_id + 1) % 3000 ==0:
                 print("take a break to give google EE some time to process ;)")
--- regression/src/process_MSB.py
+++ regression/src/process_MSB.py
@@ -25,11 +25,16 @@ EE_CRS = CRS.from_epsg(4326)
 
 MSB_no_data_countries = ["Germany", "United Kingdom", "Netherlands", "France", "Switzerland", "Ireland"]
 medium = ["Italy", "Spain"]
+MSmanually_blocked_cities = ["zaragoza"]
 good = ["Croatia", "Slovakia", "Bulgaria", "Czechia", "Romania", "Sweden", "Greece", "Austria", "Finland", "Denmark" ]
 
 MSmanually_checked_cities = {"riga": "Latvia", "bari": "Italy", "palermo": "Italy", "lisbon": "Portugal"}
 
 rename_countries = {"Czechia": "Czech_Republic"}
+
+# MSdone_cities = ["zagreb", "budapest", "riga", "bari"]
+MSdone_cities = []
+S1done_cities = []
 
 
 def write_raster_like(output, output_path, guide_tiff):
@@ -105,6 +110,7 @@ def process_data(target_data_dir,MS_downlaod_dir):
         data_pathMSB = join(each_city, "MSB")
         data_pathMSBc = join(each_city, "MSBc")
         data_pathMSBs = join(each_city, "MSBs")
+        data_pathS1 = join(each_city, "S1")
         csv_path = join(each_city, each_city.split(sep)[-1:][0] + '.csv')
         city_df = pd.read_csv(csv_path)
         ids = city_df['GRD_ID']
@@ -115,20 +121,21 @@ def process_data(target_data_dir,MS_downlaod_dir):
         classes_pathsMSB = [data_pathMSB + '/Class_' + x + '/' for x in classes_str]
         classes_pathsMSBc = [data_pathMSBc + '/Class_' + x + '/' for x in classes_str]
         classes_pathsMSBs = [data_pathMSBs + '/Class_' + x + '/' for x in classes_str]
+        classes_pathsS1 = [data_pathS1 + '/Class_' + x + '/' for x in classes_str]
         city = each_city.split("_")[-1] 
         country_name,_ = get_country_name(classes_paths[0] + str(ids[0]) + '_sen2spring.tif')
+
         # MSB Footprints
-        if country_name in MSB_no_data_countries:
-            # print("There is no MSB data for:", country_name)
-            continue
+        # if country_name in MSB_no_data_countries:
+        #     # print("There is no MSB data for:", country_name)
+        #     continue
 
         if country_name is None:
             # there are some cities where countryname== is still oke, because they are by the sea
-            if city in MSmanually_checked_cities.keys():
-                pass
+            if city in MSmanually_checked_cities.keys(): 
                 country_name = MSmanually_checked_cities[city]
-            else:
-                continue
+            # else:
+            #     continue
 
         print(country_name, ",", city)
 
@@ -137,116 +144,156 @@ def process_data(target_data_dir,MS_downlaod_dir):
             #     continue
             file_name = classes_paths[index] + str(ids[index]) + '_sen2spring.tif'
             name_MSB = str(ids[index]) + '_MSB'
-            description = name_MSB + "_" + city
-            file_name_MSBc = classes_pathsMSB[index] + str(ids[index]) + '_MSB.tif'
-            file_name_MSBc = classes_pathsMSBc[index] + str(ids[index]) + '_MSB.tif'
-            file_name_MSBs = classes_pathsMSBs[index] + str(ids[index]) + '_MSB.tif'
+            name_S1 = str(ids[index]) + '_S1'
+            descriptionMSB = name_MSB + "_" + city
+            descriptionS1 = name_S1 + "_" + city
+            file_name_MSB = classes_pathsMSB[index] + str(ids[index]) + '_MSB.tif'
+            file_name_MSBc = classes_pathsMSBc[index] + str(ids[index]) + '_MSBc.tif'
+            file_name_MSBs = classes_pathsMSBs[index] + str(ids[index]) + '_MSBs.tif'
+            file_name_S1 = classes_pathsS1[index] + str(ids[index]) + '_S1.tif'
             makedirs(classes_pathsMSBc[index], exist_ok=True)
             makedirs(classes_pathsMSBs[index], exist_ok=True)
+            makedirs(classes_pathsS1[index], exist_ok=True)
 
-            folder = "So2Satdata/" + classes_pathsMSB[index]
-            folder = folder.replace("/", "／")
-            geojsonfile = join(join(MS_downlaod_dir,folder), description+".geojson")
+            folderMSB = "So2Satdata/" + classes_pathsMSB[index]
+            folderMSB = folderMSB.replace("/", "／")
+            geojsonfile = join(join(MS_downlaod_dir,folderMSB), descriptionMSB+".geojson")
 
-            with rasterio.open(file_name) as src:
-                # band1 = src.read(1)
-                height = src.height
-                width = src.width
-                this_crs = src.crs
-                this_meta = src.meta.copy()
-
-            # get the coordinates in their local coodinate system
-            cols, rows = np.meshgrid(np.arange(width), np.arange(height))
-            xs, ys = rasterio.transform.xy(src.transform, rows, cols)
-            lons = np.array(xs)
-            lats = np.array(ys)
-            # outpoly = np.array(transform_geom( this_crs,  EE_CRS, inpoly)["coordinates"])
-            # outpoly = transform_geom( this_crs,  EE_CRS, buildingsdf["geometry"])["coordinates"]
-
-            # Transform the Buildings into the this_crs (3035) coordinate system, much better
-            x84_min = np.min(lons)
-            x84_max =  np.max(lons)
-            y84_min = np.min(lats)
-            y84_max = np.max(lats)
-
-            long_pixel = (x84_max-x84_min)/width
-            lat_pixel = (y84_max-y84_min)/height
-
-            long_start = x84_min
-            lat_start = y84_max
-        
-            long_end = long_start + long_pixel * width
-            lat_end = lat_start - lat_pixel * height # negative because the latitude dimension is flipped
-
-            long_range = np.linspace(long_start, long_end, width) #+ long_pixel/2
-            lat_range = np.linspace(lat_start, lat_end, height) #- lat_pixel/2 # negative because the latitude dimension is flipped
-            
-            # Count Buildings
-            with open(geojsonfile) as f:
-                first_line = f.readline()
-            if first_line==']}':
-                #no buildings for that patch
-                output = np.zeros((1,height, width)).astype(np.float32)
-                no_shapes = True
-            else:
-                no_shapes = False
-                buildingsdf = gpd.read_file(geojsonfile)
-                # TODO: Calculate central pixel!
-                buildingsdf["centroids"] = buildingsdf["geometry"].apply(lambda x: x.centroid) 
-                buildingsdf["c_x"] = buildingsdf["centroids"].apply(lambda x: x.x)
-                buildingsdf["c_y"] = buildingsdf["centroids"].apply(lambda x: x.y)
-
-                buildingsdf["coords_in"] = buildingsdf.apply(lambda row: {"type": "Point",  "coordinates": [row["c_x"], row["c_y"]]} , axis=1)
-
-                #  e4326 to e3035
-                buildingsdf["coords_proj"] = buildingsdf["coords_in"].apply(lambda x: np.array(transform_geom(EE_CRS, this_crs, x)["coordinates"]) )
-                buildingsdf["long"] = buildingsdf["coords_proj"].apply(lambda x: x[0])
-                buildingsdf["lat"] = buildingsdf["coords_proj"].apply(lambda x: x[1])
-
-                    
-                this_buildings = buildingsdf[np.logical_and( 
-                                                np.logical_and(buildingsdf["long"]>long_start,  buildingsdf["long"]<long_end) ,
-                                                np.logical_and(buildingsdf["lat"]<lat_start, buildingsdf["lat"]>lat_end))].copy()
-                
-                this_buildings["long_range_round"] = rounder(long_range)(buildingsdf["long"]) 
-                this_buildings["lat_range_round"] = rounder(lat_range)(buildingsdf["lat"]) 
-                # print("doing")
-
-                tiff_reader = rasterio.open(file_name, 'r', num_threads='all_cpus')
-                
-                with rasterio.open(file_name) as src:
-                    # band1 = src.read(1)
-                    this_buildings["index_coords"] =  this_buildings.apply(lambda row: src.index(row["long_range_round"],row["lat_range_round"]), axis=1)
-                    index_coords = src.index(this_buildings["long_range_round"],this_buildings["lat_range_round"])
-                
-                #TODO: filter out the nan values here
-                
-
-                this_buildings["x"] =  index_coords[0]
-                this_buildings["y"] =  index_coords[1]
-                this_buildings["x"] =  this_buildings["index_coords"].apply(lambda x: x[0])
-                this_buildings["y"] =  this_buildings["index_coords"].apply(lambda x: x[1])
-                count_df = this_buildings.groupby(["x","y"]).size().reset_index(name='building_counts')
-
-                output = np.zeros((1,height, width)).astype(np.float32)
-                output[0,count_df["x"],count_df["y"]] = count_df["building_counts"]
+            folderS1 = "So2Satdata/" + classes_pathsS1[index]
+            folderS1 = folderS1.replace("/", "／")
+            S1file = join(join(MS_downlaod_dir,folderS1), descriptionS1 + ".tif")
 
 
-                buildingsdf["valid"] = buildingsdf["geometry"].type=="Polygon"
-                buildingsdf_valid = buildingsdf[buildingsdf["valid"]]
-                shapes = buildingsdf_valid["geometry"].apply(lambda inpoly: transform_geom(EE_CRS, this_crs, inpoly))
+            #check if the MSB Footprints
+            process_MSB = True
+            if country_name in MSB_no_data_countries:
+                process_MSB = False
 
+            if city in MSdone_cities:
+                process_MSB = False
 
-            write_raster_like(output, output_path=file_name_MSBc, guide_tiff=file_name)
-            
-            this_meta.update({"count": 1, "dtype": "float32"})
-            with rasterio.open(file_name_MSBs, 'w+', **this_meta) as out:
-                if no_shapes:
-                    out.write_band(1, output[0])
+            if country_name is None:
+                # there are some cities where countryname== is still oke, because they are by the sea
+                if city in MSmanually_checked_cities.keys():
+                    pass
+                    country_name = MSmanually_checked_cities[city]
                 else:
-                    out_arr = out.read(1)
-                    burned = rasterize(shapes=shapes, fill=1, out=out_arr, transform=out.transform)
-                    out.write_band(1, burned)
+                    process_MSB = False
+            
+            if city in MSmanually_blocked_cities:
+                process_MSB = False
+
+
+
+            if process_MSB:
+
+                with rasterio.open(file_name) as src: 
+                    height = src.height
+                    width = src.width
+                    this_crs = src.crs
+                    this_meta = src.meta.copy()
+
+                # get the coordinates in their local coodinate system
+                cols, rows = np.meshgrid(np.arange(width), np.arange(height))
+                xs, ys = rasterio.transform.xy(src.transform, rows, cols)
+                lons = np.array(xs)
+                lats = np.array(ys) 
+                # Transform the Buildings into the this_crs (3035) coordinate system, much better
+                x84_min = np.min(lons)
+                x84_max =  np.max(lons)
+                y84_min = np.min(lats)
+                y84_max = np.max(lats)
+
+                long_pixel = (x84_max-x84_min)/width
+                lat_pixel = (y84_max-y84_min)/height
+
+                long_start = x84_min
+                lat_start = y84_max
+            
+                long_end = long_start + long_pixel * width
+                lat_end = lat_start - lat_pixel * height # negative because the latitude dimension is flipped
+
+                long_range = np.linspace(long_start, long_end, width) #+ long_pixel/2
+                lat_range = np.linspace(lat_start, lat_end, height) #- lat_pixel/2 # negative because the latitude dimension is flipped
+                
+                # Count Buildings
+                with open(geojsonfile) as f:
+                    first_line = f.readline()
+                if first_line==']}':
+                    #no buildings for that patch
+                    output = np.zeros((1,height, width)).astype(np.float32)
+                    no_shapes = True
+                else:
+                    no_shapes = False
+                    buildingsdf = gpd.read_file(geojsonfile)
+
+                    # Calculate central pixel and build geometry element
+                    buildingsdf["centroids"] = buildingsdf["geometry"].apply(lambda x: x.centroid) 
+                    buildingsdf["c_x"] = buildingsdf["centroids"].apply(lambda x: x.x)
+                    buildingsdf["c_y"] = buildingsdf["centroids"].apply(lambda x: x.y)
+                    buildingsdf["coords_in"] = buildingsdf.apply(lambda row: {"type": "Point",  "coordinates": [row["c_x"], row["c_y"]]} , axis=1)
+
+                    #  e4326 to e3035
+                    buildingsdf["coords_proj"] = buildingsdf["coords_in"].apply(lambda x: np.array(transform_geom(EE_CRS, this_crs, x)["coordinates"]) )
+                    buildingsdf["long"] = buildingsdf["coords_proj"].apply(lambda x: x[0])
+                    buildingsdf["lat"] = buildingsdf["coords_proj"].apply(lambda x: x[1])
+
+                    # Filter Buildings that are out of bounds
+                    this_buildings = buildingsdf[np.logical_and( 
+                                                    np.logical_and(buildingsdf["long"]>long_start,  buildingsdf["long"]<long_end) ,
+                                                    np.logical_and(buildingsdf["lat"]<lat_start, buildingsdf["lat"]>lat_end))].copy()
+                    
+                    # Round to the pixel values
+                    this_buildings["long_range_round"] = rounder(long_range)(buildingsdf["long"]) 
+                    this_buildings["lat_range_round"] = rounder(lat_range)(buildingsdf["lat"]) 
+
+                    # get the indices of the buildings in the pixel coorinate space of the target system
+                    with rasterio.open(file_name) as src: 
+                        this_buildings["index_coords"] =  this_buildings.apply(lambda row: src.index(row["long_range_round"],row["lat_range_round"]), axis=1)
+                        index_coords = src.index(this_buildings["long_range_round"],this_buildings["lat_range_round"])
+                    
+                    # count the buildings
+                    this_buildings["x"] =  index_coords[0]
+                    this_buildings["y"] =  index_coords[1]
+                    this_buildings["x"] =  this_buildings["index_coords"].apply(lambda x: x[0])
+                    this_buildings["y"] =  this_buildings["index_coords"].apply(lambda x: x[1])
+                    count_df = this_buildings.groupby(["x","y"]).size().reset_index(name='building_counts')
+
+                    output = np.zeros((1,height, width)).astype(np.float32)
+                    output[0,count_df["x"],count_df["y"]] = count_df["building_counts"]
+
+                    # to build the segmentation masks, we do:
+                    buildingsdf["valid"] = buildingsdf["geometry"].type=="Polygon"
+                    buildingsdf_valid = buildingsdf[buildingsdf["valid"]]
+                    shapes = buildingsdf_valid["geometry"].apply(lambda inpoly: transform_geom(EE_CRS, this_crs, inpoly))
+
+
+                write_raster_like(output, output_path=file_name_MSBc, guide_tiff=file_name)
+                
+                this_meta.update({"count": 1, "dtype": "float32"})
+                with rasterio.open(file_name_MSBs, 'w+', **this_meta) as out:
+                    if no_shapes:
+                        out.write_band(1, output[0])
+                    else:
+                        out_arr = out.read(1)
+                        burned = rasterize(shapes=shapes, fill=1, out=out_arr, transform=out.transform)
+                        out.write_band(1, burned)
+
+
+            # Also put the Sentinel-1 tile with the correct projection in the correct folder
+            # Check if downloading of S1 is needed
+            process_S1 = True 
+            if city in S1done_cities:
+                process_S1 = False
+
+            if process_S1:
+                # transform_geom(EE_CRS, this_crs, inpoly)
+                
+                with rasterio.open(S1file) as src: 
+                    s1raster = src.read((1,2))
+
+                write_raster_like(s1raster, output_path=file_name_S1, guide_tiff=file_name)
+
 
             # /scratch2/metzgern/HAC/code/So2SatPOP/data/GEEexport/So2Satdata／／scratch2／metzgern／HAC／code／So2SatPOP／data／So2Sat_POP_Part1／train／00745_20865_zagreb／MSB／Class_0／
 
